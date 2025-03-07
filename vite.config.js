445af3eb0ff8daf3571583da5fe549d020a490d5
bhawani47 <120445101+bhawani47@users.noreply.github.com>
2025-03-07 14:50:28 +0530

    Fix bugs

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,10 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+  server: {
+    historyApiFallback: true,  // Ensures proper SPA routing
+  },
+  build: {
+    outDir: "dist",
+  }
 })
